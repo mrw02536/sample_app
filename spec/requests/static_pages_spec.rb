2a3b8c7ed947c28736ae5ecc7195af36bd460da5
mrw <mrichard.weiss@gmail.com>
2013-09-29 11:16:16 -0400

    Added contact page and test

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -43,14 +43,14 @@ describe "Static pages" do
 
   describe "Contact page" do
 
-    it "should have the content ('Contact Us')"  do
+    it "should have the content 'Contact Us'" do
       visit '/static_pages/contact'
-      expect(page).to.have_content('Contact Us')
+      expect(page).to have_content('Contact Us')
     end
 
-  it "should have the title ('Contact Us')" do
-     visit '/static_pages/contact'
-     expect(page).to.have.have_title("Ruby on Rails Tutorial Sample App | Contact Us")
+    it "should have the title 'Contact Us'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact Us")
     end
   end
  end
