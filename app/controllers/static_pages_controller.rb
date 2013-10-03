8dd2ab2419c2825276d98847d92fda75fa28c17f
mrw <mrichard.weiss@gmail.com>
2013-10-03 16:30:21 -0400

    Added titles to application.html.erb

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,14 +1,18 @@
 class StaticPagesController < ApplicationController
   def home
+    @title = "Home"
   end
 
   def help
+    @title = "Help"
   end
 
   def contact
+    @title = "Contact Us"
   end
 
   def about
+    @title = "About Us"
   end
 
 end
